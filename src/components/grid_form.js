44f6c3b0ee63c57bd252a9ebe1ddae855666b113
bobrobcpp <king.roob@gmail.com>
2017-09-27 13:32:14 +0100

    Update grid_form component to use one method for dimension change rather than calculating height and width in seperate functions

--- src/components/grid_form.js
+++ src/components/grid_form.js
@@ -5,20 +5,30 @@ class GridForm extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
-      gridHeight: '',
-      gridWidth: ''
+      gridHeight: '0',
+      gridWidth: '0'
     };
   }
   render(){
     return(
       <div className="grid-form">
-        Grid Height
-        <input type="number"/>
-        Grid Width
-        <input type="number"/>
+
+
+
+        {this.state.gridHeight}
+        <input type="number" min="0" onChange={event => this.onDimensionChange(event.target.value, this.state.gridWidth)} />
+        {this.state.gridWidth}
+        <input type="number" min="0" onChange={event => this.onDimensionChange(this.state.gridHeight, event.target.value)} />
 
       </div>
       );
+  }
+
+  onDimensionChange(height, width){
+    this.setState({
+      gridHeight: height,
+      gridWidth: width
+    });
   }
 
 
